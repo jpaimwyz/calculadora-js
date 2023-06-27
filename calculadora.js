b4bb8d53396b9c5707fb2666537951a048b9f7b0
Jussana Paim <126022889+jpaimwyz@users.noreply.github.com>
2023-06-27 18:16:24 +0100

    Recriação do código js

--- calculadora.js
+++ calculadora.js
@@ -1,54 +1,67 @@
-/* 
-Quando alguém pega uma calculadora, eles podem apertar: 
-1. um número (0-9);
-2. um operador (+, -, *, /);
-3. tecla decimal;
-4. a tecla de igual;
-5. a tecla de limpar;
- */
+const calcular = (n1, operador, n2) => {
+    let resultado = ''
+
+    if(operador === 'adicionar'){
+        resultado = parseFloat(n1) + parseFloat(n2)
+    } else if(operador === 'subtrair'){
+        resultado = parseFloat(n1) - parseFloat(n2)
+    } else if(operador === 'multiplicar'){
+        resultado = parseFloat(n1) * parseFloat(n2)
+    } else if(operador === 'dividir'){
+        resultado = parseFloat(n1) / parseFloat(n2)
+    }
+
+    return resultado
+}
+
+
 const calculadora = document.querySelector('.calculadora')
-const teclas = document.querySelector('.calculadora_teclas')
+const teclas = calculadora.querySelector('.calculadora_teclas')
 const tela = document.querySelector('.calculadora_tela')
 
-//Esse código permite ouvir todas as teclas, já que são filhas da classe .calculadora_teclas.
 teclas.addEventListener('click', e =>{
     if(e.target.matches('button')){
-        //o código abaixo determina qual tecla foi clicada usando o atributo data-action
         const tecla = e.target
-        const accao = tecla.dataset.action
-        /*Para fazer o código para mostrar os números e exibir na tela, precisamos de duas coisas: 
-        1. qual tecla foi clicada;
-        2. o número atual exibido;
-        */
-        const conteudoTecla = tecla.textContent
-        const numeroNaTela = tela.textContent
+        const action = tecla.dataset.action
+        const teclaConteudo = tecla.textContent
+        const numNaTela = tela.textContent
+        const previousKeyType = calculadora.dataset.previousKeyType
 
-        //Condição que verifica o atributo data-action, caso não tenha esse atributo, é considerada uma tecla númerica.
-        if(!accao){
-            if(numeroNaTela === '0'){
-            tela.textContent = conteudoTecla
-        } else{
-            tela.textContent = numeroNaTela + conteudoTecla
+        Array.from(tecla.parentNode.children).forEach(t => t.classList.remove('pressionado'))
+        
+        if(!action){
+            if(numNaTela === '0' || previousKeyType === 'operador'){
+                tela.textContent = teclaConteudo
+            } else {
+                tela.textContent = numNaTela + teclaConteudo
+            }
+        }
+        
+        if(
+            action === 'adicionar' || 
+            action === 'subtrair' || 
+            action === 'multiplicar' || 
+            action === 'dividir'
+            ){
+            tecla.classList.add('pressionado')
+            calculadora.dataset.previousKeyType = 'operador'
+            calculadora.dataset.firstValue = numNaTela
+            calculadora.dataset.operador = action
+            }
+       
+        
+        if(action === 'decimal'){
+            tela.textContent = numNaTela + '.'
+        }
+        if(action === 'limpar'){
+            tela.textContent = '0'
+        }
+        if(action === 'calcular'){
+            const pValor = calculadora.dataset.firstValue
+            const operacao = calculadora.dataset.operador
+            const sValor = numNaTela
+
+            tela.textContent = calcular(pValor, operacao, sValor)
         }
     }
-        //Se ela tiver um data-action, que seja um dos que está abaixo, então, é um operador.
-        if(
-            accao === "adicionar" ||
-            accao === "subtrair" ||
-            accao === "multiplicar" ||
-            accao === "dividir"
-        ){
-            console.log('Tecla de Operador')
-        }
-        //É o mesmo processo para as teclas de decimal, limpar e calcular
-        if(accao === "decimal"){
-            tela.textContent = numeroNaTela + '.'
-        }
-        if(accao === "limpar"){
-            console.log('Tecla limpar')
-        }
-        if(accao === "calcular"){
-            console.log('Tecla calcular')
-        }
-}
 })
