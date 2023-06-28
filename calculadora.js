cf96d0e801f19b748f3f0e9d606c669a9cc6b9c5
Jussana Paim <126022889+jpaimwyz@users.noreply.github.com>
2023-06-28 21:36:04 +0100

    Calculadora parcialmente concluída

--- calculadora.js
+++ calculadora.js
@@ -1,92 +1,110 @@
-const calcular = (n1, operador, n2) => {
+/* Vou refazer o códigp do zero. */
+/* Função calcular - criada depois */
+const calcular = (n1, operator, n2) => {
     let resultado = ''
-
-    if(operador === 'adicionar'){
+    if(operator === 'adicionar'){
         resultado = parseFloat(n1) + parseFloat(n2)
-    } else if(operador === 'subtrair'){
+    } else if(operator === 'subtrair'){
         resultado = parseFloat(n1) - parseFloat(n2)
-    } else if(operador === 'multiplicar'){
+    }
+    else if(operator === 'multiplicar'){
         resultado = parseFloat(n1) * parseFloat(n2)
-    } else if(operador === 'dividir'){
+    }
+    else if(operator === 'dividir'){
         resultado = parseFloat(n1) / parseFloat(n2)
     }
-
     return resultado
 }
 
+
 const calculadora = document.querySelector('.calculadora')
 const teclas = calculadora.querySelector('.calculadora_teclas')
-const tela = document.querySelector('.calculadora_tela')
+const ecrã = calculadora.querySelector('.calculadora_tela')
 
-teclas.addEventListener('click', e =>{
+teclas.addEventListener('click', e => {
     if(e.target.matches('button')){
         const tecla = e.target
-        const action = tecla.dataset.action
-        const teclaConteudo = tecla.textContent
-        const numNaTela = tela.textContent
-        const previousKeyType = calculadora.dataset.previousKeyType
+        const ação = tecla.dataset.action
+        const conteudoTecla = tecla.textContent
+        const numeroNaTela = ecrã.textContent
+        const teclaAnterior = calculadora.dataset.previousKeyType
 
-        Array.from(tecla.parentNode.children).forEach(t => t.classList.remove('pressionado'))
-        
-        if(!action){
-            if(numNaTela === '0' || previousKeyType === 'operador'){
-                tela.textContent = teclaConteudo
+        Array.from(tecla.parentNode.children).forEach(k => k.classList.remove('pressionado'))
+        //Números
+        if(!ação){
+            if(numeroNaTela === '0' || 
+            teclaAnterior === 'operador' || teclaAnterior === 'calcular'){
+                ecrã.textContent = conteudoTecla
             } else {
-                tela.textContent = numNaTela + teclaConteudo
+                ecrã.textContent = numeroNaTela + conteudoTecla
             }
             calculadora.dataset.previousKeyType = 'numero'
         }
-        
-        if( action === 'adicionar' || 
-            action === 'subtrair' || 
-            action === 'multiplicar' || 
-            action === 'dividir')
-            {
-            const pValor = calculadora.dataset.firstValue
-            const operacao = calculadora.dataset.operador
-            const sValor = numNaTela
 
-            if(pValor && operacao && previousKeyType !== 'operador'){
-                const vCalculado = calcular(pValor, operacao, sValor)
-                tela.textContent = vCalculado
+        //Operadores
+        if(ação === 'adicionar' || ação === 'subtrair' || ação === 'multiplicar' || ação === 'dividir'){
+            const primeiroValor = calculadora.dataset.firstValue
+            const operador = calculadora.dataset.operator
+            const segundoValor = numeroNaTela
 
-                calculadora.data.firstValue = vCalculado
+            if(primeiroValor && operador && teclaAnterior !== 'operador' && teclaAnterior !== 'calcular'){
+                const valorCalculado = calcular(primeiroValor, operador, segundoValor)
+                ecrã.textContent = valorCalculado
+                calculadora.dataset.firstValue = valorCalculado
             } else {
-                calculadora.dataset.firstValue = numNaTela
+                calculadora.dataset.firstValue = numeroNaTela
             }
 
             tecla.classList.add('pressionado')
             calculadora.dataset.previousKeyType = 'operador'
-            calculadora.dataset.operador = action
+            calculadora.dataset.operator = ação
+
         }
-        
-        if(action === 'decimal'){
-            if(!numNaTela.includes('.')){
-                tela.textContent = numNaTela + '.'
-            } else if(previousKeyType === 'operador'){
-                tela.textContent = '0.'
+
+        //Decimal, Limpar e Calcular
+        if(ação === 'decimal'){
+            if(!numeroNaTela.includes('.')){
+                ecrã.textContent = numeroNaTela + '.'
+            }
+            else if(teclaAnterior === 'operador' || teclaAnterior === 'calcular'){
+                ecrã.textContent = '0.'
             }
             calculadora.dataset.previousKeyType = 'decimal'
         }
 
-        if(action === 'limpar'){
-            tela.textContent = '0'
-            calculadora.dataset.previousKeyType = 'limpar'
+        if(ação === 'limpar'){
+            if(tecla.textContent === 'AC'){
+                calculadora.dataset.firstValue = ''
+                calculadora.dataset.modValue = ''
+                calculadora.dataset.operator = ''
+                calculadora.dataset.previousKeyType = ''
+            } else{
+                tecla.textContent = 'AC'
+            }
+            ecrã.textContent = 0
+            calculadora.dataset.previousKeyType = ' limpar'
         }
-        if(action === 'calcular'){
-            const pValor = calculadora.dataset.firstValue
-            const operacao = calculadora.dataset.operador
-            const sValor = numNaTela
+        if(ação !== 'limpar'){
+            const btnLimpar = calculadora.querySelector('[data-action=limpar]')
+            btnLimpar.textContent = 'CE'
+        }
 
-            if(pValor){
-                if(previousKeyType === 'calcular'){
-                    pValor = numNaTela
+        if(ação === 'calcular'){
+            let primeiroValor = calculadora.dataset.firstValue
+            const operador = calculadora.dataset.operator
+            let segundoValor = numeroNaTela
+
+            if(primeiroValor){
+                if(teclaAnterior === 'calcular'){
+                    primeiroValor = numeroNaTela
+                    segundoValor = calculadora.dataset.modValue
                 }
-                
-                tela.textContent = calcular(pValor, operacao, sValor)
+                ecrã.textContent = calcular(primeiroValor, operador, segundoValor)
             }
 
+            calculadora.dataset.modValue = segundoValor
             calculadora.dataset.previousKeyType = 'calcular'
         }
+        
     }
 })
